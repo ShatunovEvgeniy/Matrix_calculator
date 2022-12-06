d379465197a2be7bada3c0c11504c943d762b746
ShatunovEvgeniy <shatunov868@gmail.com>
2022-12-06 18:09:25 +0300

    Files in process, window can be changed

--- other_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef OTHERS_FUNCTIONS_H
-#define OTHERS_FUNCTIONS_H
-
-#include <vector>
-
-template <typename T>
-std::vector<T> slicing(std::vector<T> const& v, int X, int Y); // slice of a vector
-
-#endif // OTHERS_FUNCTIONS_H
