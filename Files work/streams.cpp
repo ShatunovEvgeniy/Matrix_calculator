e01e49e1188da5ad86877a8636779c9abc22dc8d
G00sya <118297271+G00sya@users.noreply.github.com>
2022-12-09 15:52:26 +0300

    Add files via upload

--- Files work/streams.cpp
+++ Files work/streams.cpp
@@ -55,109 +55,110 @@ string get_first_string(istream& ist)
     return word;
 }
 
-void write(Matrix m, const string& out_name)
+void write(Matrix m, ofstream& ofs)
 {
-    ofstream ofs{out_name};
-    if (!ofs)
-        throw runtime_error("Can't open output file");
     ofs<<m;
 }
 
-void write_det(long double det, const string& out_name)
-{
-    ofstream ofs{out_name};
-    if (!ofs)
-        throw runtime_error("Can't open output file");
-    ofs<<det;
-}
 
 void calculate(const string& name, const string& out_name){
     ifstream ist{name};
     if (!ist)
         throw runtime_error("bad file");
+    ofstream ofs{out_name};
+    if (!ofs)
+        throw runtime_error("Can't open output file");
 
-    vector<string> aviable {"det", "triangle", "diagonalization", "transpon"};
+    vector<string> aviable {"det", "triangle", "transpose"};
     vector<string> znaki {"+", "-", "*"};
 
-    string word = get_first_string(ist);
-    while (ist.peek() == ' ' || ist.peek() == '\n')
-        ist.get();
+    string word;
 
-    if (in_aviable(aviable, word))
+    while (true)
     {
-        vector<vector<long double>> v;
-        fill(ist, v);
-        Matrix m{v};
-        if (word == "det")
-        {
-            if (m.get_length() == m.get_width())
-            {
-                Sqr_matrix sq{m};
-                write_det(sq.det(), out_name);
-            }
-            else
-                throw runtime_error("Not sqr matrix");
-        }
-        if (word == "triangle")
-        {
-            if (m.get_length() == m.get_width())
-            {
-                Sqr_matrix sq{m};
-                write(sq.triangle(), out_name);
-            }
-            else
-                throw runtime_error("Not sqr matrix");
-        }
-        else if (word == "diagonalization")
-        {
-            if (m.get_length() == m.get_width())
-            {
-                Sqr_matrix sq{m};
-                write(sq.diagonalization(), out_name);
-            }
-        }
-        else if (word == "transpon")
-        {
-            write(m.T(), out_name);
-        }
-    }
-
-    else if (in_aviable(znaki, word))
-    {
-        vector<vector<long double>> v1;
-        vector<vector<long double>> v2;
-
-        fill(ist, v1);
-
-        while (ist.peek() == '\n' || ist.peek() == ' ')
+        while (ist.peek() == ' ' || ist.peek() == '\n')
             ist.get();
 
-        fill(ist, v2);
+        word = get_first_string(ist);
 
-        Matrix m1{v1};
-        Matrix m2{v2};
+        while (ist.peek() == ' ' || ist.peek() == '\n')
+            ist.get();
 
-        if (word == "+")
+        if (in_aviable(aviable, word))
         {
-            m1 += m2;
-            write(m1, out_name);
+            vector<vector<long double>> v;
+            fill(ist, v);
+            Matrix m{v};
+            if (word == "det")
+            {
+                if (m.get_length() == m.get_width())
+                {
+                    Sqr_matrix sq{v};
+                    long double det = sq.det();
+                    ofs<<det;
+                    ofs<<'\n';
+                }
+                else
+                    throw runtime_error("Not sqr matrix");
+            }
+            if (word == "triangle")
+            {
+                if (m.get_length() == m.get_width())
+                {
+                    Sqr_matrix sq{v};
+                    write(sq.triangle(), ofs);
+                }
+                else
+                    throw runtime_error("Not sqr matrix");
+            }
+            else if (word == "transpose")
+            {
+                write(m.T(), ofs);
+            }
         }
 
-        else if (word == "-")
+        else if (in_aviable(znaki, word))
         {
-            m1 -= m2;
-            write(m1, out_name);
+            vector<vector<long double>> v1;
+            vector<vector<long double>> v2;
+
+            fill(ist, v1);
+
+            while (ist.peek() == '\n' || ist.peek() == ' ')
+                ist.get();
+
+            fill(ist, v2);
+
+            Matrix m1{v1};
+            Matrix m2{v2};
+
+            if (word == "+")
+            {
+                m1 += m2;
+                write(m1, ofs);
+            }
+
+            else if (word == "-")
+            {
+                m1 -= m2;
+                write(m1, ofs);
+            }
+
+            else if (word == "*")
+            {
+                m1 *= m2;
+                write(m1, ofs);
+            }
         }
 
-        else if (word == "*")
-        {
-            m1 *= m2;
-            write(m1, out_name);
-        }
-    }
-
-    else
+        else
         {
             throw runtime_error("Bad input");
         }
+
+        while (ist.peek() == ' ' || ist.peek() == '\n')
+            ist.get();
+        if (ist.eof())
+            break;
+    }
 }
